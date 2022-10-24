b6b838348e38f6a03f9c650f2e2c94e3c12d435e
Andrew Campi <63204545+andrewcampi@users.noreply.github.com>
2022-10-24 14:56:37 -0400

    Add files via upload

---
+++ pywebiosecure/__version__.py
@@ -0,0 +1,8 @@
+__package__ = 'pywebiosecure'
+__description__ = 'A fork of pywebio 1.7 that supports HTTPS.'
+__url__ = 'https://pywebio.readthedocs.io'
+__version__ = "1.0.0"
+__version_info__ = (1, 0, 0, 0)
+__author__ = 'AndrewCampi'
+__author_email__ = 'acampi.pypi@gmail.com'
+__license__ = 'MIT'
